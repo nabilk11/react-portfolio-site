eedea00b8f98799583595b31c6dc65bd0be6f9aa
nabilk11 <khan.nabiln11@gmail.com>
2022-01-27 02:49:29 -0500

    added topbar to main app page, and converted app to jsx file.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Topbar from "./components/topbar/topbar";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Topbar/>
+      <div className="sections"></div>
+    </div>
+  );
+}
+
+export default App;
