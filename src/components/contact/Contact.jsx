0301acdffaea67552492ca885330dbdd92d4ac6b
nabilk11 <khan.nabiln11@gmail.com>
2022-01-27 02:45:53 -0500

    added initial imports/exports to component jsx pages, and basic background color styling for components.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,1 +1,11 @@
+import React from 'react';
+import "./contact.scss"
 
+
+export default function Contact() {
+  return (
+  <div className="contact">
+
+  </div>
+  )
+}
---
+++ src/components/work/Work.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import "./work.scss"
+
+export default function work() {
+  return (
+  <div className="work">
+
+  </div>
+  )
+}
