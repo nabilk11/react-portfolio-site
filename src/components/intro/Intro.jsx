91eb20c572b2df19f959726418e84c800836c116
nabilk11 <khan.nabiln11@gmail.com>
2022-01-29 14:25:37 -0500

    added intro image lebron placeholder. added intro text. added next section arrow and blinking animation styling.

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -4,7 +4,21 @@ import "./intro.scss"
 export default function Intro() {
   return (
   <div className="intro" id="intro"> 
-  Intro test text
+  <div className="left">
+    <div className="imgContainer">
+      <img src="assets/lebron_lal_trans.png" alt="" />
+    </div>
+  </div>
+  <div className="right">
+    <div className="wrapper">
+      <h2>Hello! I'm</h2>
+      <h1>Nabil Khan!</h1>
+      <h3>Full Stack Software Engineer | Leader | Introvert<span></span></h3>
+    </div>
+    <a href="#portfolio">
+      <img src="assets/downarrow.jpg" alt="" />
+    </a>
+  </div>
   </div>
   )
 }
