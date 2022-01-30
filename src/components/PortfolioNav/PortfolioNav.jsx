8566e6bcda2c6a6d69e4996a4322026dfcfee65a
nabilk11 <khan.nabiln11@gmail.com>
2022-01-30 15:51:41 -0500

    created basic portfolio layout and new portfolio navigation component

---
+++ src/components/PortfolioNav/PortfolioNav.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './portfolionav.scss'
+
+export default function PortfolioNav({title}) {
+  return (
+  
+  <li className="portfolioNav">
+      {title}
+  </li>
+  )
+}
+
--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,9 +1,68 @@
 import React from 'react';
 import "./portfolio.scss"
+import PortfolioNav from '../PortfolioNav/PortfolioNav'
 
 export default function Portfolio() {
+// portfolio nav list items array
+const list = [
+  {
+      id: "featured",
+      title: "Featured",
+  },
+  {
+      id: "web",
+      title: "Web App",
+  },
+  {
+      id: "fullstack",
+      title: "Full Stack",
+  },
+  {
+      id: "design",
+      title: "Design",
+  },
+  {
+      id: "branding",
+      title: "Branding",
+  },
+  
+];
+
   return (
   <div className="portfolio" id="portfolio">
+    <h1>Portfolio</h1>
+    <ul>
+      {list.map(item => {
+       return <PortfolioNav title={item.title}/>
+      })}
+    </ul>
+    <div className="container">
+      <div className="item">
+        <img src="assets/LFG_Screenshot.png" alt="" />
+        <h3>CRUD Rest App</h3>
+      </div>
+      <div className="item">
+        <img src="assets/LFG_Screenshot.png" alt="" />
+        <h3>CRUD Rest App</h3>
+      </div>
+      <div className="item">
+        <img src="assets/LFG_Screenshot.png" alt="" />
+        <h3>CRUD Rest App</h3>
+      </div>
+      <div className="item">
+        <img src="assets/LFG_Screenshot.png" alt="" />
+        <h3>CRUD Rest App</h3>
+      </div>
+      <div className="item">
+        <img src="assets/LFG_Screenshot.png" alt="" />
+        <h3>CRUD Rest App</h3>
+      </div>
+      <div className="item">
+        <img src="assets/LFG_Screenshot.png" alt="" />
+        <h3>CRUD Rest App</h3>
+      </div>
+    </div>
+    
 
   </div>
   )
